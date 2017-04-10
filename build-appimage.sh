1fe984abd7b07edb27bc2597d53e7e3efc9c0c75
Samantha McVey <samantham@posteo.net>
2017-04-09 22:01:37 -0700

    Update script

--- build-appimage.sh
+++ build-appimage.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-sudo -p mkdir /rsu
+APP=perl6
+ID=org.perl6.rakudo
+
+PWD=$(pwd)
+sudo mkdir -p -v /rsu
 sudo chown $(whoami):$(whoami) /rsu || exit
 #cd /rsu
-tar -xf rakudo-star-*.tar.gz || exit
+wget http://rakudo.org/downloads/star/rakudo-star-latest.tar.gz
+tar -xf rakudo-star*.tar.gz || exit
 cd rakudo-star* || exit
 perl ./Configure.pl --prefix="/rsu" --backends=moar --gen-moar || exit
 make || exit
@@ -12,6 +17,8 @@ find . -type f | xargs -I '{}' sed -i -e 's|/rsu|././|g' '{}'
 mkdir -p usr
 mv * ./usr
 echo "Now you need to fix usr/bin/perl6 script"
-
-APP=perl6
-ID=org.perl6.rakudo
+cp $PWD/perl6 ./usr/bin/perl6
+chmod +x ./usr/bin/perl6
+mkdir -v "$APP.AppImage" || exit
+cd install || exit
+cp "$PWD/$ID.desktop" "./$APP.AppImage"
