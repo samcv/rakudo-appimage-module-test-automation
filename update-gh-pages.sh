83c2831cc0dfde508fff96239adbf69db289c12c
Samantha McVey <samantham@posteo.net>
2017-04-10 17:15:24 -0700

    More github pages work

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,36 +1,43 @@
 #!/usr/bin/env bash
 # Adapted from script by Yuriy Tymchuk 2013
+# and here https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 # https://sleepycoders.blogspot.com/2013/03/sharing-travis-ci-generated-files.html
-
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update gh-pages\n"
+TARGET_BRANCH="gh-pages"
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$TARGET_BRANCH"  ]; then
+  printf -e "Starting to update gh-pages\n"
 
   #copy data we're interested in to other place
-  mkdir -p $HOME/staging
+  mkdir -p "$HOME/staging"
   APPIMAGENAME=$(basename "$(find . -name '*.AppImage')")
   if [ "$BLEAD" ]; then
-      mv "$APPIMAGENAME" "blead-$APPIMAGENAME"
+    NAME="blead"
   else
-      mv "$APPIMAGENAME" "star-$APPIMAGENAME"
+    NAME='star'
   fi
+  mv "$APPIMAGENAME" "$NAME-${P6SCRIPT}${APPIMAGENAME}"
   cp *.AppImage $HOME/staging
 
   #go to home and setup git
-  cd $HOME
+  cd -- "$HOME" || echo "Couldn't cd into $HOME"; exit 1
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
-
-  #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/samcv/rakudo-appimages.git  gh-pages > /dev/null
-
-  #go into diractory and copy data we're interested in to that directory
+    # Save some useful information
+  REPO=`git config remote.origin.url`
+  SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+  SHA=`git rev-parse --verify HEAD`
+  git clone $REPO gh-pages
   cd gh-pages
+  git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+  # Copy our files from staging to the repo
   cp -Rf $HOME/staging/* .
-
+  if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+  fi
   #add, commit and push files
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git push -fv origin gh-pages
 
-  echo -e "Done magic with coverage\n"
+  echo -e "Done magic with push\n"
 fi
